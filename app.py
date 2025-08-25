98add8a0ab93f6e1745a0e97590edc44d99f4458
cristoffergomes-sudo <cristoffer.gomes@escola.pr.gov.br>
2025-08-25 09:41:06 -0300

    crie as listas

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 @lista de convidados que foram convidados para a festa
 
+convidados_da_festa = ['Nayelli','Igor','gabriel','Bnianca", 'Daniela']
+
+#Dicionario vazio para armazenar o status de presença de cada pessoa
+status_oresenca = {}
+
+lista_de_chegadas = ['Igor','Gabriel','Bianca',Daniela']
